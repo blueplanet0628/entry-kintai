25208c42446080af495679775e56a0a921416d4c
keiichi_nakamura <keiichi.nakamura@full-sailing.com>
2023-11-18 13:55:04 +0900

    ログイン画面

---
+++ app/login/page.tsx
@@ -0,0 +1,28 @@
+import {
+    LoginButton,
+    LogoutButton,
+} from "@/app/components/buttons";
+import {getServerSession} from "next-auth/next";
+import {options} from "@/lib/options";
+
+export default async function Home() {
+    const session = await getServerSession(options)
+    const user = session?.user // NOTE: ログインしていなければnull.
+
+    return (
+        <main
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "70vh",
+            }}
+        >
+            <div>
+                <div>{`${JSON.stringify(user)}`}</div>
+                {user ? <div>Logged in</div> : <div>Not logged in</div>}
+                {user ? <LogoutButton/> : <LoginButton/>}
+            </div>
+        </main>
+    );
+}
---
+++ lib/options.ts
@@ -0,0 +1,72 @@
+import type {NextAuthOptions} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prismadb from '@/lib/prismadb'
+
+export const options: NextAuthOptions = {
+        debug: true,
+        session: {strategy: "jwt"},
+        providers: [
+            CredentialsProvider({
+                    name: "Email",
+                    credentials: {
+                        email: {
+                            label: "Email",
+                            type: "email",
+                            placeholder: "example@example.com",
+                        },
+                        password: {label: "Password", type: "password"},
+                    },
+                    // メルアド認証処理
+                    async authorize(credentials) {
+                        const users = [
+                            {id: "1", email: "user1@example.com", password: "password1"},
+                            {id: "2", email: "user2@example.com", password: "password2"},
+                            {id: "3", email: "abc@abc", password: "123"},
+                        ];
+
+                        const user = await prismadb.user.findUnique({
+                          where: { email: credentials?.email },
+                        })
+
+                        //const user = users.find(user => user.email === credentials?.email);
+
+                        if (user && user?.password === credentials?.password) {
+                            return {id: user.id, name: user.email, email: user.email, role: "admin"};
+                        } else {
+                            return null;
+                        }
+                    }
+                }
+            ),
+        ],
+        callbacks: {
+            jwt: async ({token, user, account, profile, isNewUser}) => {
+                // 注意: トークンをログ出力してはダメです。
+                // console.log('in jwt', {user, token, account, profile})
+
+                if (user) {
+                    token.user = user;
+                    const u = user as any
+                    token.role = u.role;
+                }
+                if (account) {
+                    token.accessToken = account.access_token
+                }
+                return token;
+            },
+            session: ({session, token}) => {
+                // console.log("in session", {session, token});
+                token.accessToken
+                return {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        role: token.role,
+                    },
+                };
+            },
+        }
+    }
+;
