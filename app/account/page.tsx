7e356167765f3a525731e0103aa9d1b342124186
shiraishi <shohei.shiraishi@full-sailing.com>
2023-11-18 16:27:15 +0900

    add layout

---
+++ app/account/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Account() {
+	return (
+		<div>
+			<h1>Hello, Next.js!</h1>
+			<p>This is a simple page created with Next.js.</p>
+		</div>
+	);
+}
