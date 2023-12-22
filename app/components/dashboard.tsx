5aa765986c73d54e0c990bd7d703006f66f8a2ae
keiichi_nakamura <keiichi.nakamura@full-sailing.com>
2023-12-23 03:12:12 +0900

    タイムカードTOPページ修正

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -16,6 +16,8 @@ import List from "@mui/material/List";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import * as React from "react";
 import { mainListItems, secondaryListItems } from "./listitems";
 
@@ -102,82 +104,84 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
 	};
 
 	return (
-		<ThemeProvider theme={defaultTheme}>
-			<Box sx={{ display: "flex" }}>
-				<CssBaseline />
-				<AppBar position="absolute" open={open}>
-					<Toolbar
-						sx={{
-							pr: "24px", // keep right padding when drawer closed
-						}}
-					>
-						<IconButton
-							edge="start"
-							color="inherit"
-							aria-label="open drawer"
-							onClick={toggleDrawer}
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<ThemeProvider theme={defaultTheme}>
+				<Box sx={{ display: "flex" }}>
+					<CssBaseline />
+					<AppBar position="absolute" open={open}>
+						<Toolbar
 							sx={{
-								marginRight: "36px",
-								...(open && { display: "none" }),
+								pr: "24px", // keep right padding when drawer closed
 							}}
 						>
-							<MenuIcon />
-						</IconButton>
-						<Typography
-							component="h1"
-							variant="h6"
-							color="inherit"
-							noWrap
-							sx={{ flexGrow: 1 }}
+							<IconButton
+								edge="start"
+								color="inherit"
+								aria-label="open drawer"
+								onClick={toggleDrawer}
+								sx={{
+									marginRight: "36px",
+									...(open && { display: "none" }),
+								}}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography
+								component="h1"
+								variant="h6"
+								color="inherit"
+								noWrap
+								sx={{ flexGrow: 1 }}
+							>
+								勤怠管理システム
+							</Typography>
+							<IconButton color="inherit">
+								<Badge badgeContent={4} color="secondary">
+									<NotificationsIcon />
+								</Badge>
+							</IconButton>
+						</Toolbar>
+					</AppBar>
+					<Drawer variant="permanent" open={open}>
+						<Toolbar
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "flex-end",
+								px: [1],
+							}}
 						>
-							勤怠管理システム
-						</Typography>
-						<IconButton color="inherit">
-							<Badge badgeContent={4} color="secondary">
-								<NotificationsIcon />
-							</Badge>
-						</IconButton>
-					</Toolbar>
-				</AppBar>
-				<Drawer variant="permanent" open={open}>
-					<Toolbar
+							<IconButton onClick={toggleDrawer}>
+								<ChevronLeftIcon />
+							</IconButton>
+						</Toolbar>
+						<Divider />
+						<List component="nav">
+							{mainListItems}
+							<Divider sx={{ my: 1 }} />
+							{secondaryListItems}
+						</List>
+					</Drawer>
+					<Box
+						component="main"
 						sx={{
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "flex-end",
-							px: [1],
+							backgroundColor: (theme) =>
+								theme.palette.mode === "light"
+									? theme.palette.grey[100]
+									: theme.palette.grey[900],
+							flexGrow: 1,
+							height: "100vh",
+							overflow: "auto",
 						}}
 					>
-						<IconButton onClick={toggleDrawer}>
-							<ChevronLeftIcon />
-						</IconButton>
-					</Toolbar>
-					<Divider />
-					<List component="nav">
-						{mainListItems}
-						<Divider sx={{ my: 1 }} />
-						{secondaryListItems}
-					</List>
-				</Drawer>
-				<Box
-					component="main"
-					sx={{
-						backgroundColor: (theme) =>
-							theme.palette.mode === "light"
-								? theme.palette.grey[100]
-								: theme.palette.grey[900],
-						flexGrow: 1,
-						height: "100vh",
-						overflow: "auto",
-					}}
-				>
-					<Toolbar />
-					<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-						{children}
-						<Copyright sx={{ pt: 4 }} />
-					</Container>
+						<Toolbar />
+						<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+							{children}
+							<Copyright sx={{ pt: 4 }} />
+						</Container>
+					</Box>
 				</Box>
-			</Box>
-		</ThemeProvider>
+			</ThemeProvider>
+		</LocalizationProvider>
 	);
 }
