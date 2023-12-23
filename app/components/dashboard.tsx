e7d8d4f1c4199c5b03315a910d922253d1017aef
shiraishi <shohei.shiraishi@full-sailing.com>
2023-12-23 16:19:41 +0900

    fix dashboard

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -2,9 +2,8 @@
 
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import MenuIcon from "@mui/icons-material/Menu";
-import NotificationsIcon from "@mui/icons-material/Notifications";
+import { Paper } from "@mui/material";
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import Badge from "@mui/material/Badge";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -15,6 +14,8 @@ import List from "@mui/material/List";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import * as React from "react";
 import { mainListItems, secondaryListItems } from "./listitems";
 
@@ -84,80 +85,77 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
 
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<Box sx={{ display: "flex" }}>
-				<CssBaseline />
-				<AppBar position="absolute" open={open}>
-					<Toolbar
-						sx={{
-							pr: "24px", // keep right padding when drawer closed
-						}}
-					>
-						<IconButton
-							edge="start"
-							color="inherit"
-							aria-label="open drawer"
-							onClick={toggleDrawer}
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<Box sx={{ display: "flex" }}>
+					<CssBaseline />
+					<AppBar position="absolute" open={open}>
+						<Toolbar
 							sx={{
-								marginRight: "36px",
-								...(open && { display: "none" }),
+								pr: "24px", // keep right padding when drawer closed
 							}}
 						>
-							<MenuIcon />
-						</IconButton>
-						<Typography
-							component="h1"
-							variant="h6"
-							color="inherit"
-							noWrap
-							sx={{ flexGrow: 1 }}
+							<IconButton
+								edge="start"
+								color="inherit"
+								aria-label="open drawer"
+								onClick={toggleDrawer}
+								sx={{
+									marginRight: "36px",
+									...(open && { display: "none" }),
+								}}
+							>
+								<MenuIcon />
+							</IconButton>
+							<Typography
+								component="h1"
+								variant="h6"
+								color="inherit"
+								noWrap
+								sx={{ flexGrow: 1 }}
+							>
+								勤怠管理システム
+							</Typography>
+						</Toolbar>
+					</AppBar>
+					<Drawer variant="permanent" open={open}>
+						<Toolbar
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "flex-end",
+								px: [1],
+							}}
 						>
-							勤怠管理システム
-						</Typography>
-						<IconButton color="inherit">
-							<Badge badgeContent={4} color="secondary">
-								<NotificationsIcon />
-							</Badge>
-						</IconButton>
-					</Toolbar>
-				</AppBar>
-				<Drawer variant="permanent" open={open}>
-					<Toolbar
+							<IconButton onClick={toggleDrawer}>
+								<ChevronLeftIcon />
+							</IconButton>
+						</Toolbar>
+						<Divider />
+						<List component="nav">
+							{mainListItems}
+							<Divider sx={{ my: 1 }} />
+							{secondaryListItems}
+						</List>
+					</Drawer>
+					<Box
+						component="main"
 						sx={{
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "flex-end",
-							px: [1],
+							backgroundColor: (theme) =>
+								theme.palette.mode === "light"
+									? theme.palette.grey[100]
+									: theme.palette.grey[900],
+							flexGrow: 1,
+							height: "100vh",
+							overflow: "auto",
 						}}
 					>
-						<IconButton onClick={toggleDrawer}>
-							<ChevronLeftIcon />
-						</IconButton>
-					</Toolbar>
-					<Divider />
-					<List component="nav">
-						{mainListItems}
-						<Divider sx={{ my: 1 }} />
-						{secondaryListItems}
-					</List>
-				</Drawer>
-				<Box
-					component="main"
-					sx={{
-						backgroundColor: (theme) =>
-							theme.palette.mode === "light"
-								? theme.palette.grey[100]
-								: theme.palette.grey[900],
-						flexGrow: 1,
-						height: "100vh",
-						overflow: "auto",
-					}}
-				>
-					<Toolbar />
-					<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-						{children}
-					</Container>
+						<Toolbar />
+						<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+							<Paper sx={{ padding: 2 }}>{children}</Paper>
+						</Container>
+					</Box>
 				</Box>
-			</Box>
+			</LocalizationProvider>
 		</ThemeProvider>
 	);
 }
