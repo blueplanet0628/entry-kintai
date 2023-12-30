4b640d35dd79632cccda2c083080856201976617
shiraishi <shohei.shiraishi@full-sailing.com>
2023-12-30 22:41:25 +0900

    シード更新

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,33 @@ import { Role } from "@prisma/client";
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma/prismadb";
 
+const PASSWORD = "entrypassword";
+
+const generrateAttendance = (userId: number, shopIds: number[]) => {};
+
+function getCombinations<T>(elements: T[], k: number): T[][] {
+	const result: T[][] = [];
+
+	function generateCombinations(currentCombination: T[], start: number) {
+		if (currentCombination.length === k) {
+			result.push([...currentCombination]);
+			return;
+		}
+
+		for (let i = start; i < elements.length; i++) {
+			currentCombination.push(elements[i]);
+			generateCombinations(currentCombination, i + 1);
+			currentCombination.pop();
+		}
+	}
+
+	generateCombinations([], 0);
+
+	return result;
+}
+
 async function main() {
-	const password = await bcrypt.hash("password", 12);
+	const password = await bcrypt.hash(PASSWORD, 12);
 
 	// 会社
 	await prisma.company.upsert({
@@ -16,29 +41,51 @@ async function main() {
 		},
 		update: {},
 	});
-
-	// 店舗
-	await prisma.shop.upsert({
+	await prisma.company.upsert({
 		where: {
-			id: 1,
+			id: 2,
 		},
 		create: {
-			id: 1,
-			companyId: 1,
-			code: "0001",
-			type: 1,
-			name: "店舗1",
-			phoneNumber1: "000-0000-0000",
-			addressPostcode: "000-0000",
-			addressCity: "豊島区",
-			addressPrefecture: "東京都",
-			addressBlock: "東",
-			shiftPeriod: 7,
-			shiftDeadline: 10,
-			isEnabled: true,
+			id: 2,
+			name: "株式会社ENTRYカフェ",
 		},
 		update: {},
 	});
+	await prisma.company.upsert({
+		where: {
+			id: 2,
+		},
+		create: {
+			id: 2,
+			name: "株式会社ENTRYゲーム",
+		},
+		update: {},
+	});
+
+	// 店舗
+	for (let i = 1; i <= 3; i++) {
+		await prisma.shop.upsert({
+			where: {
+				id: i,
+			},
+			create: {
+				id: i,
+				companyId: 1,
+				code: `000${i}`,
+				type: 1,
+				name: `ENTRY店舗${i}`,
+				phoneNumber1: "000-0000-0000",
+				addressPostcode: "000-0000",
+				addressCity: "豊島区",
+				addressPrefecture: "東京都",
+				addressBlock: `東-${i}`,
+				shiftPeriod: 7,
+				shiftDeadline: 10,
+				isEnabled: true,
+			},
+			update: {},
+		});
+	}
 
 	// ユーザー（管理者）
 	await prisma.user.upsert({
@@ -73,49 +120,59 @@ async function main() {
 		update: {},
 	});
 	// ユーザー（被雇用者）
-	await prisma.user.upsert({
-		where: {
-			id: 3,
-		},
-		create: {
-			id: 3,
-			companyId: 1,
-			name: "ENTRY一般ユーザー",
-			email: "staff@entry-kintai.com",
-			role: Role.USER,
-			password,
-			isEnabled: true,
-		},
-		update: {},
-	});
+	for (let i = 1; i <= 13; i++) {
+		await prisma.user.upsert({
+			where: {
+				id: 2 + i,
+			},
+			create: {
+				id: 2 + i,
+				companyId: 1,
+				name: `ENTRY一般ユーザー${i}`,
+				email: `staff_${i}@entry-kintai.com`,
+				role: Role.USER,
+				password,
+				isEnabled: true,
+			},
+			update: {},
+		});
+	}
 
 	// 店舗 - ユーザー中間テーブル
-	await prisma.shopUser.upsert({
-		where: {
-			shopId_userId: {
-				shopId: 1,
-				userId: 2,
+	for (let i = 1; i <= 15; i++) {
+		await prisma.shopUser.upsert({
+			where: {
+				shopId_userId: {
+					shopId: 1,
+					userId: i,
+				},
 			},
-		},
-		create: {
-			shopId: 1,
-			userId: 2,
-		},
-		update: {},
-	});
-	await prisma.shopUser.upsert({
-		where: {
-			shopId_userId: {
+			create: {
 				shopId: 1,
-				userId: 3,
+				userId: i,
 			},
-		},
-		create: {
-			shopId: 1,
-			userId: 3,
-		},
-		update: {},
-	});
+			update: {},
+		});
+	}
+
+	// 店舗間交通費
+	const shopFares = getCombinations([1, 2, 3], 2);
+	for (const shopFare of shopFares) {
+		await prisma.fareBetweenShop.upsert({
+			where: {
+				shop1Id_shop2Id: {
+					shop1Id: shopFare[0],
+					shop2Id: shopFare[1],
+				},
+			},
+			create: {
+				shop1Id: shopFare[0],
+				shop2Id: shopFare[1],
+				fare: 1000,
+			},
+			update: {},
+		});
+	}
 }
 
 main()
